cf640aab7c6cfa5af89df723a99234a04c7c5254
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-18 10:19:19 +0200

    Use Howler to play sounds

--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
+import { Howl } from 'https://cdn.skypack.dev/howler@2.2.3?min';
+
 export function disableContextMenu() {
-  document.addEventListener("contextmenu", (event) => event.preventDefault());
+  document.addEventListener('contextmenu', (event) => event.preventDefault());
 }
 
 export async function recordAudio(onTranscribe) {
@@ -8,11 +10,11 @@ export async function recordAudio(onTranscribe) {
   mediaRecorder.start();
 
   const audioChunks = [];
-  mediaRecorder.addEventListener("dataavailable", (event) => {
+  mediaRecorder.addEventListener('dataavailable', (event) => {
     audioChunks.push(event.data);
   });
 
-  mediaRecorder.addEventListener("stop", async () => {
+  mediaRecorder.addEventListener('stop', async () => {
     const audioBlob = new Blob(audioChunks);
     const text = await speechToText(audioBlob);
     onTranscribe(text);
@@ -26,9 +28,9 @@ export async function recordAudio(onTranscribe) {
 // -----------------------------------------------------------------------------
 
 export async function askGpt(messages) {
-  const response = await fetch("/gpt", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const response = await fetch('/gpt', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ messages }),
   });
   const text = await response.json();
@@ -37,22 +39,24 @@ export async function askGpt(messages) {
 
 export async function speechToText(audioBlob) {
   const formData = new FormData();
-  formData.append("file", audioBlob);
+  formData.append('file', audioBlob);
 
-  const response = await fetch("/stt", { method: "POST", body: formData });
+  const response = await fetch('/stt', { method: 'POST', body: formData });
   const text = await response.json();
   return text;
 }
 
 export async function textToSpeech(text) {
-  const response = await fetch("/tts", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const response = await fetch('/tts', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ text }),
   });
 
   const audioBlob = await response.blob();
   const audioUrl = URL.createObjectURL(audioBlob);
-  const audio = new Audio(audioUrl);
-  audio.play();
+
+  const sound = new Howl({ src: [audioUrl], format: ['mp3'] });
+  sound.rate(1.2);
+  sound.play();
 }

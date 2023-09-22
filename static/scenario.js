a6d7371c215e3fd644092b579f123b95ee41eb9d
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-22 22:43:26 +0200

    Add scenario page

---
+++ static/scenario.js
@@ -0,0 +1,46 @@
+export const scenario = [
+  {
+    name: '01. INTRO',
+    text: "Bonjour les enfants, je m'appelle Timio, je suis un robot.\nJ'ai créé une puce électronique qui permet de rendre les autres robots intelligents comme vous et moi.\nMais avant d'en faire profiter tous les autres robots, j'ai très envie de faire une chasse au trésor avec vous.\nJe vous propose donc de résoudre des énigmes et de participer à des épreuves, qui vous permettront de trouver la puce que j'ai cachée.\nUne récompense vous attend à la fin de cette aventure.\n\nSi vous êtes prêts, j'ai envoyé par la poste votre premier indice, trouvez-le.",
+  },
+  {
+    name: "02. AVANT LA COURSE D'OBSTACLES",
+    text: "C'est parti pour un peu d'exercice physique. Un par un, vous allez devoir traverser cette course d'obstacles. Attendez-vous à l'arrivée, et vous recevrez un nouvel indice pour continuer.",
+  },
+  {
+    name: "03. APRES LA COURSE D'OBSTACLES",
+    text: "Bravo les enfants, quelle magnifique course ! Vous avez bien mérité que je vous dévoile l'indice suivant.\nIl se trouve caché quelque part autour de vous, cherchez bien, il n'est pas loin.",
+  },
+  {
+    name: '04. AVANT LE PARCOURS DE VOITURE',
+    text: "J'adore les voitures ! Chacun de vous va devoir faire un tour de circuit avec ma voiture télécommandée, et vous recevrez un nouvel indice à la fin. Mais avant de commencer, je crois avoir un petit problème avec cette voiture : je n'arrive pas à la faire avancer. Est-ce que vous pouvez m'aider ? Il me semble avoir laissé les batteries quelque part par ici. Cherchez bien.",
+  },
+  {
+    name: '05. APRES LE PARCOURS DE VOITURE',
+    text: "Bravo, les enfants ! Je vous ai envoyé un nouvel indice par avion pour continuer. Il devrait arriver d'ici quelques secondes.",
+  },
+  {
+    name: '06. AVANT LE PARCOURS DU SALON',
+    text: "J'ai besoin de m'assurer que vous êtes en forme. Voici donc quelques nouveaux obstacles à franchir chacun à votre tour, avant de passer à la suite.",
+  },
+  {
+    name: '07. APRES LE PARCOURS DU SALON',
+    text: "Bravo à tous ! Maintenant, j'ai besoin de vous pour réparer un copain à moi.\nSur la table se trouvent plusieurs outils. L'un d'entre eux va disparaitre, et vous en aurez besoin pour réparer mon ami qui se trouve dans le couloir, un peu plus loin.\nVous devez mémoriser ces outils, vous avez 15 secondes, puis fermez les yeux.",
+  },
+  {
+    name: '08. APRES LA REPARATION DU ROBOT',
+    text: "Merci les enfants, vous avez bien travaillé. Pour continuer l'aventure, rendez-vous sur la petite terrasse !",
+  },
+  {
+    name: '09. APRES LE CHAMBOULE TOUT',
+    text: 'Bien joué !\nIl devrait y avoir une enveloppe cachée quelque part sur cette terrasse, cherchez bien.\nDedans se trouve un nouvel indice pour continuer.',
+  },
+  {
+    name: '10. (optionel) APRES LE SALON',
+    text: 'Bravo, bien joué tout le monde !\nMaintenant, rendez-vous dehors pour un dernier effort, vous êtes très proches du but !',
+  },
+  {
+    name: '11. APRES LA DERNIERE EPREUVE',
+    text: "Félicitations à vous tous ! Vous êtes dignes de récupérer la puce électronique que j'ai créé, afin de donner l'intelligence aux autres robots.\nCette puce se trouve cachée dans une pièce de la maison. Elle est indiquée dans le tableau que voici, saurez-vous le déchiffrer ?",
+  },
+];
